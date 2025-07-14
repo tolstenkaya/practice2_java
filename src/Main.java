32790fab4a37a92b2e1506057f7037f58763e914
tolstenkaya <o.tutova.82@gmail.com>
2025-07-14 20:52:56 +0300

    1 Task, Array

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,30 @@ public class Main{
         //1. Дано целое число:
         //int num = 10203304506;
         //Выведите в консоль позиции всех цифр 0 в этом числе, за исключением первой и последней.
-        long num = 10203304506L;
-        String num_to_str = Long.toString(num);
-
-        int amount = 0;
-        for(int i=0;i<num_to_str.length();i++)
-        {
-            if(num_to_str.charAt(i)=='0'){
-                amount++;
-            }
-        }
-
-        int[] arr_indexes = new int[amount];
-        int n=0;
-        for (int i=0; i<num_to_str.length();i++){
-            if(num_to_str.charAt(i)=='0')
-            {
-                arr_indexes[n]=i;
-                n++;
-            }
-        }
-
-        for(int i=1;i<arr_indexes.length-1;i++){
-            System.out.print(arr_indexes[i]);
-        }
+//        long num = 10203304506L;
+//        String num_to_str = Long.toString(num);
+//
+//        int amount = 0;
+//        for(int i=0;i<num_to_str.length();i++)
+//        {
+//            if(num_to_str.charAt(i)=='0'){
+//                amount++;
+//            }
+//        }
+//
+//        int[] arr_indexes = new int[amount];
+//        int n=0;
+//        for (int i=0; i<num_to_str.length();i++){
+//            if(num_to_str.charAt(i)=='0')
+//            {
+//                arr_indexes[n]=i;
+//                n++;
+//            }
+//        }
+//
+//        for(int i=1;i<arr_indexes.length-1;i++){
+//            System.out.print(arr_indexes[i]);
+//        }
 
     }
 }
