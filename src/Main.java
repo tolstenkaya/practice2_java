0edf92953c2f453b8b4658540f6023a86d4837ce
tolstenkaya <o.tutova.82@gmail.com>
2025-07-15 21:35:16 +0300

    7-11 Tasks, Array, String

--- src/Main.java
+++ src/Main.java
@@ -96,6 +96,94 @@ public class Main{
 //        }
 //        System.out.println(new_arr);
 
-        //7.
+        //7.Дана строка с целыми числами, разделенными пробелами
+        //String str = "123 456 789";
+        //Найдите сумму чисел из этой строки.
+//        String str = "123 456 789";
+//        String[] nums_str = str.split(" ");
+//        int sum = 0;
+//        for(String num_str : nums_str){
+//            int num = Integer.parseInt(num_str);
+//            sum+=num;
+//        }
+//        System.out.println("Sum: "+sum);
+
+        //8. Дана строка со словами:
+        //String str = "word1 word2 word3";
+        //Получите первые N слов из этой строки.
+
+//        String str ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+//                "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
+//                "when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+//                "It has survived not only five centuries, but also the leap into electronic typesetting, " +
+//                "remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset " +
+//                "sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like " +
+//                "Aldus PageMaker including versions of Lorem Ipsum.";
+//
+//        String[] words = str.split(" ");
+//        Scanner scan = new Scanner(System.in);
+//
+//        int n;
+//        do{
+//            System.out.print("Enter amount of words (N): ");
+//            n=scan.nextInt();
+//            if(n>str.length()){
+//                System.out.println("N is bigger than amount of the words! Try again.");
+//            }
+//        }while(n>str.length());
+//
+//        for(int i=0;i<n;i++){
+//            System.out.println(words[i]);
+//        }
+
+        //9. Дан массив символов, содержащий цифры:
+        //chars[] {'1', '2', '3', '4', '5'};
+        //Слейте элементы этого массива в целое число:
+        //int 12345;
+//        char[] char_nums = new char[]{'1','2','3','4','5'};
+//        String num_str = new String(char_nums);
+//        int num = Integer.parseInt(num_str);
+//        System.out.println(num);
+
+
+        //10. Дана некоторая строка:
+        //"AbCdE"
+        //Смените регистр букв этой строки на противоположный. В нашем случае должно получится следующее:
+        //"aBcDe"
+//        String str = "AbCdE";
+//
+//        char[] symbols = str.toCharArray();
+//        for(int i=0;i<symbols.length;i++){
+//            if(symbols[i]>='A' && symbols[i]<='Z'){
+//                symbols[i]+=32;
+//            }
+//            else if(symbols[i]>='a' && symbols[i]<='z'){
+//                symbols[i]-=32;
+//            }
+//        }
+//        String str_new = new String(symbols);
+//        System.out.println(str_new);
+
+        //11. Дана дата в следующем формате:
+        //String date = "31-12-2025";
+        //Преобразуйте ее в следующий формат:
+        //String date = "31 декабря 2025";
+//        String date = "31-12-2025";
+//        String[] arr_date = date.split("-");
+//
+//        String[] months = new String[]{"January", "February", "March",
+//                "April", "May", "June",
+//                "July", "August", "September",
+//                "October", "November", "December"};
+//        String date_to_str = "";
+//        for(int i=0;i<arr_date.length;i++){
+//            if(i==1){
+//                date_to_str+=months[Integer.parseInt(arr_date[i])-1];
+//            }else{
+//                date_to_str+=arr_date[i];
+//            }
+//            date_to_str+=" ";
+//        }
+//        System.out.println(date_to_str);
     }
 }
